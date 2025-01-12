6dd3dd1499d75e59e05961f29fc4f5e4464ba4ba
Shishir Gopinath <11451052+hvsg@users.noreply.github.com>
2025-01-12 05:55:27 +0000

    Add Vertex and Factor types

---
+++ examples/optimize.cu
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <numeric>
+#include <glso/core.hpp>
+
+int main(void) {
+
+    // Create graph
+
+
+    // Create vertices
+
+    // Create edges
+
+    std::cout << "Optimizing!" << std::endl;
+
+    return 0;
+}
--- include/glso/core.hpp
+++ include/glso/core.hpp
@@ -2,9 +2,68 @@
 
 namespace glso {
 
-class Graph {
+
+template <typename T, int D>
+class VertexDescriptor {
+private:
+
+
+public:
+
+void update(T* vertex, const T* update) const;
+
+};
+
+// V is the number of vertices
+// O is the dimension of the observation
+// E is the dimension of the error vector
+template <typename T, int V, int O, int E>
+class FactorDescriptor {
+public:
+
+// T** get_vertices(Graph* graph);
+// const get_vertex_types() 
+
+void compute_error(const T** vertices, const T* obs, T* error) const;
 
 };
 
 
+class Vertex {
+    private:
+
+    size_t id;
+
+    public:
+
+    void set_id(size_t id) {
+        this->id = id;
+    }
+
+};
+
+class Factor {
+
+
+};
+
+class Graph {
+
+    public:
+
+    void add_vertex_descriptor();
+
+    // void add_vertex(Vertex & vertex) {
+        
+    // }
+
+};
+
+class Optimizer {
+
+
+};
+
 }
+
+
