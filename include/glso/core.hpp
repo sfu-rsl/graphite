be122b40ed4d2e5341b88ad9e9a82bf69001aa67
Shishir Gopinath <11451052+hvsg@users.noreply.github.com>
2025-01-16 00:43:19 +0000

    Add base descriptor classes

--- include/glso/core.hpp
+++ include/glso/core.hpp
@@ -3,20 +3,6 @@
 
 namespace glso {
 
-template <typename T>
-class VertexDescriptor {
-public:
-    virtual ~VertexDescriptor() {};
-};
-
-template <typename T>
-class FactorDescriptor {
-public:
-    virtual ~FactorDescriptor() {};
-
-    virtual void error_func(const T** vertices, const T* obs, T* error) = 0;
-    virtual bool use_autodiff() = 0;
-};
 
 template<typename T>
 class GraphVisitor {
@@ -26,18 +12,58 @@ public:
         // Do something with the factor's error function
         F::error_func();
     }
+
+    template<typename V>
+    void apply_step() {
+        V::update();
+    }
+};
+
+template <typename T>
+class BaseVertexDescriptor {
+public:
+    virtual ~BaseVertexDescriptor() {};
+
+    virtual void update(const T* delta) = 0;
+    virtual void visit_update(GraphVisitor<T>& visitor) = 0;
+};
+
+template <typename T, template <typename> class Derived>
+class VertexDescriptor : public BaseVertexDescriptor<T> {
+public:
+    virtual ~VertexDescriptor() {};
+    
+    void visit_update(GraphVisitor<T>& visitor) override {
+        visitor.template apply_step<Derived<T>>();
+    }
+};
+
+template <typename T>
+class BaseFactorDescriptor {
+public:
+    virtual ~BaseFactorDescriptor() {};
+
+    virtual void error_func(const T** vertices, const T* obs, T* error) = 0;
+    virtual bool use_autodiff() = 0;
+    virtual void visit_error(GraphVisitor<T>& visitor) = 0;
+
+};
+
+template <typename T, template <typename> class Derived>
+class FactorDescriptor : public BaseFactorDescriptor<T> {
+public:
+
+    void visit_error(GraphVisitor<T>& visitor) override {
+        visitor.template compute_error<Derived<T>>();
+    }
 };
 
 // Templated derived class for AutoDiffFactorDescriptor using CRTP
 template <typename T, template <typename> class Derived>
-class AutoDiffFactorDescriptor : public FactorDescriptor<T> {
+class AutoDiffFactorDescriptor : public FactorDescriptor<T, Derived> {
 public:
     virtual bool use_autodiff() override {
         return true;
-    }
-
-    void visit_error(GraphVisitor<T>& visitor) {
-        visitor.template compute_error<Derived<T>>();
     }
 };
 
@@ -62,16 +88,16 @@ class Graph {
 
     private:
 
-    std::vector<VertexDescriptor<T>*> vertex_descriptors;
-    std::vector<FactorDescriptor<T>*> factor_descriptors;
+    std::vector<BaseVertexDescriptor<T>*> vertex_descriptors;
+    std::vector<BaseFactorDescriptor<T>*> factor_descriptors;
 
     public:
 
-    void add_vertex_descriptor(VertexDescriptor<T>* descriptor) {
+    void add_vertex_descriptor(BaseVertexDescriptor<T>* descriptor) {
         vertex_descriptors.push_back(descriptor);
     }
 
-    void add_factor_descriptor(FactorDescriptor<T>* descriptor) {
+    void add_factor_descriptor(BaseFactorDescriptor<T>* descriptor) {
         factor_descriptors.push_back(descriptor);
     }
 
