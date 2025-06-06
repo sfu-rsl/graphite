2cbe0a002836228ec2baca6cfdfb3b84b371d91e
Shishir Gopinath <11451052+hvsg@users.noreply.github.com>
2025-06-06 21:28:34 +0000

    Clean up solver

--- include/glso/solver.hpp
+++ include/glso/solver.hpp
@@ -52,7 +52,7 @@ public:
     T *b = graph->get_b().data().get();
     size_t dim_h = graph->get_hessian_dimension();
 
-    thrust::fill(thrust::device, x, x + dim_h, 0);
+    thrust::fill(thrust::device, x, x + dim_h, 0.0);
 
     size_t dim_r = 0;
     v1.resize(factor_descriptors.size());
@@ -61,30 +61,15 @@ public:
     }
 
     v1.resize(dim_r);
-    thrust::fill(v1.begin(), v1.end(), 0);
+    thrust::fill(v1.begin(), v1.end(), 0.0);
 
     v2.resize(dim_h);
-    thrust::fill(v2.begin(), v2.end(), 0);
+    thrust::fill(v2.begin(), v2.end(), 0.0);
 
     // Compute residual
     r.resize(dim_h); // dim h because dim(r) = dim(Ax) = dim(b)
-    thrust::fill(r.begin(), r.end(), 0);
-
-    // 1. First compute Jx
-    T *v1_ptr = v1.data().get();
-    for (size_t i = 0; i < factor_descriptors.size(); i++) {
-      factor_descriptors[i]->visit_Jv(visitor, v1_ptr, x);
-      v1_ptr += factor_descriptors[i]->get_residual_size();
-    }
-    cudaDeviceSynchronize();
-
-    // 2. Then compute v2 = J^T v1
-    v1_ptr = v1.data().get();
-    for (size_t i = 0; i < factor_descriptors.size(); i++) {
-      factor_descriptors[i]->visit_Jtv(visitor, v2.data().get(), v1_ptr);
-      v1_ptr += factor_descriptors[i]->get_residual_size();
-    }
-    cudaDeviceSynchronize();
+    thrust::copy(thrust::device, graph->get_b().begin(),
+                 graph->get_b().end(), r.begin());
 
     // 3. Add damping factor
     // v2 += damping_factor*diag(H)*x
@@ -101,22 +86,14 @@ public:
     T max_diag = static_cast<T>(1.0e32);
     clamp(dim_h, min_diag, max_diag, diag.data().get());
 
-    damp_by_factor(dim_h, v2.data().get(), damping_factor, diag.data().get(),
-                   x);
-
-    // 4. Finally r = b - v2
-    axpy(dim_h, r.data().get(), (T)-1.0, (const T *)v2.data().get(), b);
-
     cudaDeviceSynchronize();
 
     // Rescale r
     y.resize(dim_h);
     auto rnorm = thrust::inner_product(thrust::device, r.begin(), r.end(),
                                        r.begin(), static_cast<T>(0.0));
-    // if (rnorm > 1e-16) {
     rnorm = std::sqrt(rnorm);
     auto scale = 1.0 / rnorm;
-    // scale = std::clamp(scale, 1.0e0, 1.0e0);
     rescale_vec<T>(dim_h, y.data().get(), scale, r.data().get());
     // Apply preconditioner
     preconditioner->precompute(visitor, vertex_descriptors, factor_descriptors,
@@ -134,72 +111,14 @@ public:
     // 1. First compute dot(r, z)
     T rz = (T)thrust::inner_product(r.begin(), r.end(), z.begin(),
                                     static_cast<T>(0.0));
-    // T rz_0 = rz;
-    // T rz_0;
-
-    // if constexpr (std::is_same<S, ghalf>::value) {
-    //   rz_0 = CUDART_INF_FP16;
-    // } else {
-    //   rz_0 = std::numeric_limits<S>::infinity();
-    // }
 
     T rz_0 = std::numeric_limits<T>::infinity();
-    // T rz_0 = rz;
-
-    /*
-    // print r
-    // std::cout << "Initial residual r: ";
-    std::vector<T> r_host(dim_h);
-    thrust::copy(r.begin(), r.end(), r_host.begin());
-    // std::cout << "Initial residual r (first 10): ";
-    // for (size_t i = 0; i < std::min(dim_h, size_t(10)); ++i) {
-    //   std::cout << (T)r_host[i] << " ";
-    // }
-    // std::cout << std::endl;
-
-    std::vector<T> z_host(dim_h);
-    thrust::copy(z.begin(), z.end(), z_host.begin());
-    // std::cout << "Initial preconditioned residual z (first 10): ";
-    // for (size_t i = 0; i < std::min(dim_h, size_t(10)); ++i) {
-    //   std::cout << (T)z_host[i] << " ";
-    // }
-    // std::cout << std::endl;
-
-    std::vector<T> p_host(dim_h);
-    thrust::copy(p.begin(), p.end(), p_host.begin());
-    // std::cout << "Initial search direction p (first 10): ";
-    // for (size_t i = 0; i < std::min(dim_h, size_t(10)); ++i) {
-    //   std::cout << (T)p_host[i] << " ";
-    // }
-    // std::cout << std::endl;
-
-    // check all three for NaN or Inf by iterating
-    for (size_t i = 0; i < dim_h; ++i) {
-      if (std::isnan((T)r_host[i]) || std::isinf((T)r_host[i])) {
-      std::cerr << "NaN or Inf detected in initial residual r at index " << i
-            << ": value = " << (T)r_host[i] << std::endl;
-      return false;
-      }
-      if (std::isnan((T)z_host[i]) || std::isinf((T)z_host[i])) {
-      std::cerr << "NaN or Inf detected in initial preconditioned residual z at
-    index " << i
-            << ": value = " << (T)z_host[i] << std::endl;
-      return false;
-      }
-      if (std::isnan((T)p_host[i]) || std::isinf((T)p_host[i])) {
-      std::cerr << "NaN or Inf detected in initial search direction p at index "
-    << i
-            << ": value = " << (T)p_host[i] << std::endl;
-      return false;
-      }
-    }
-    */
 
     for (size_t k = 0; k < max_iter; k++) {
 
       // 2. Compute v1 = Jp
-      thrust::fill(v1.begin(), v1.end(), 0);
-      v1_ptr = v1.data().get(); // reset
+      thrust::fill(v1.begin(), v1.end(), 0.0);
+      auto v1_ptr = v1.data().get(); // reset
       for (size_t i = 0; i < factor_descriptors.size(); i++) {
         factor_descriptors[i]->visit_Jv(visitor, v1_ptr, p.data().get());
         v1_ptr += factor_descriptors[i]->get_residual_size();
@@ -207,7 +126,7 @@ public:
       // cudaDeviceSynchronize();
 
       // 3. Compute v2 = J^T v1
-      thrust::fill(v2.begin(), v2.end(), 0);
+      thrust::fill(v2.begin(), v2.end(), 0.0);
       v1_ptr = v1.data().get(); // reset
       for (size_t i = 0; i < factor_descriptors.size(); i++) {
         factor_descriptors[i]->visit_Jtv(visitor, v2.data().get(), v1_ptr);
@@ -218,7 +137,6 @@ public:
       // v2 += damping_factor*diag(H)*p
       damp_by_factor(dim_h, v2.data().get(), damping_factor, diag.data().get(),
                      p.data().get());
-      // cudaDeviceSynchronize();
 
       // 4. Compute alpha = dot(r, z) / dot(p, v2)
       T alpha = (rz) / thrust::inner_product(p.begin(), p.end(), v2.begin(),
@@ -233,12 +151,9 @@ public:
 
       rnorm = (T)thrust::inner_product(thrust::device, r.begin(), r.end(),
                                        r.begin(), static_cast<T>(0.0));
-      // if (rnorm > 1e-16) {
       rnorm = std::sqrt(rnorm);
       scale = 1.0 / rnorm;
-      // scale = std::clamp(scale, 1e0, 1.0e0);
       rescale_vec<T>(dim_h, y.data().get(), scale, r.data().get());
-      // }
 
       // Apply preconditioner again
       thrust::fill(z.begin(), z.end(), 0);
@@ -246,74 +161,15 @@ public:
       T rz_new = thrust::inner_product(r.begin(), r.end(), z.begin(),
                                        static_cast<T>(0.0));
 
-      /*
-      std::cout << "printing first 10 values of r and z after iteration " << k
-      << std::endl;
-      // print r
-      thrust::copy(r.begin(), r.end(), r_host.begin());
-      std::cout << "Residual r (first 10): ";
-      for (size_t i = 0; i < std::min(dim_h, size_t(10)); ++i) {
-        std::cout << (T)r_host[i] << " ";
-      }
-      std::cout << std::endl;
-      // print z
-      thrust::copy(z.begin(), z.end(), z_host.begin());
-      std::cout << "Preconditioned residual z (first 10): ";
-      for (size_t i = 0; i < std::min(dim_h, size_t(10)); ++i) {
-        std::cout << (T)z_host[i] << " ";
-      }
-      std::cout << std::endl;
-
-      thrust::copy(v1.begin(), v1.begin() + 10, r_host.begin());
-      std::cout << "v1 (Jp) (first 10): ";
-      for (size_t i = 0; i < std::min(dim_r, size_t(10)); ++i) {
-        std::cout << (T)r_host[i] << " ";
-      }
-      std::cout << std::endl;
-
-      thrust::copy(v2.begin(), v2.end(), r_host.begin());
-      std::cout << "v2 (J^T v1) (first 10): ";
-      for (size_t i = 0; i < std::min(dim_h, size_t(10)); ++i) {
-        std::cout << (T)r_host[i] << " ";
-      }
-      std::cout << std::endl;
-
-      std::cout << "alpha: " << static_cast<T>(alpha) << std::endl;
-
-
-
-      std::cout << "Iteration " << k
-                << ", rz_new: " << static_cast<T>(rz_new)
-                << ", rz: " << static_cast<T>(rz)
-                << ", rz_0: " << static_cast<T>(rz_0) << std::endl;
-
-                */
-
-      // 7. Check termination criteria
-      // if (sqrt(rz_new / rz_0) < tol) {
-      //   // std::cout << "Converged at iteration " << k << std::endl;
-      //   break;
-      // }
-
       // if (rz_new > rejection_ratio * rz_0) {
       if (std::abs(rz_new) > rejection_ratio * rz_0) {
         thrust::copy(thrust::device, x_backup.begin(), x_backup.end(), x);
-        // std::cout << "Diverged at iteration " << k
-        //           << ", resetting to previous x." << std::endl;
-        //           std::cout << "rz_new: " << static_cast<T>(rz_new) << ",
-        //           rz_0: " << static_cast<T>(rz_0) << ", rejection_ratio: " <<
-        //           rejection_ratio << std::endl;
         break;
       }
-      // rz_0 = std::min(rz_0, rz_new);
       rz_0 = std::min(rz_0, std::abs(rz_new));
 
       // 8. Compute beta
       T beta = rz_new / rz;
-      // std::cout << "Iteration " << k << ", rz_new: " <<
-      // static_cast<T>(rz_new)
-      //           << ", rz_0: " << static_cast<T>(rz_0) << ", beta: "
-      //           << static_cast<T>(beta) << std::endl;
       rz = rz_new;
 
       // 9. Update p
@@ -321,9 +177,6 @@ public:
       cudaDeviceSynchronize();
 
       if (std::abs(static_cast<T>(rz_new)) < tol) {
-        // std::cout << "Converged at iteration " << k << std::endl;
-        // std::cout << "rz_new: " << static_cast<T>(rz_new) << ", tol: " << tol
-        //           << std::endl;
         break;
       }
     }
