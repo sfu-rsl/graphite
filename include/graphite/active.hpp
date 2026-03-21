d2f0b6e162b8aa142112b107fc5b9ffeed5cc446
Shishir Gopinath <11451052+hvsg@users.noreply.github.com>
2026-03-21 07:31:34 +0000

    Make build_active_indices inline to fix build errors

--- include/graphite/active.hpp
+++ include/graphite/active.hpp
@@ -21,8 +21,29 @@ is_vertex_active(const uint8_t *active_state, const size_t vertex_id) {
 }
 
 // Returns the number of active constraints and fills the active_indices vector
-size_t build_active_indices(const thrust::device_vector<uint8_t> &active,
-                            thrust::device_vector<size_t> &active_indices,
-                            const size_t count, const uint8_t level);
+inline size_t
+build_active_indices(const thrust::device_vector<uint8_t> &active,
+                     thrust::device_vector<size_t> &active_indices,
+                     const size_t count, const uint8_t level) {
+
+  // Count active constraints
+  const size_t active_count =
+      thrust::count_if(thrust::device, active.begin(), active.end(),
+                       [level] __device__(const uint8_t a) {
+                         return is_factor_active(a, level);
+                       });
+
+  // Resize active indices to the number of active constraints
+  active_indices.clear();
+  active_indices.resize(active_count);
+
+  // Fill active indices with the indices of active constraints
+  thrust::copy_if(thrust::device, thrust::make_counting_iterator<size_t>(0),
+                  thrust::make_counting_iterator<size_t>(count), active.begin(),
+                  active_indices.begin(), [level] __device__(const uint8_t a) {
+                    return is_factor_active(a, level);
+                  });
+  return active_count;
+}
 
 } // namespace graphite
